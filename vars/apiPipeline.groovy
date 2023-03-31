91a44c5ff9145c5d9fc11622e6f0eed0b7d4e36b
José Vásquez <josevasquezc6@gmail.com>
2023-03-30 23:30:42 -0300

    Add on api shared librarie

---
+++ vars/apiPipeline.groovy
@@ -0,0 +1,62 @@
+def call(String project, String artifactName, boolean withTest){
+  pipeline {
+      agent any
+      environment{
+          TELEGRAM_BOT_TOKEN = credentials('telegram_bot_token')
+          TELEGRAM_CHANNEL = credentials('telegram_channel_id')
+          PATH_TEST = "./${project}Test/${project}Test.csproj"
+          PATH_PRJ = "./${project}/${project}.csproj"
+          PATH_PUB = "./${project}/bin/Release/net6.0/publish"
+          ARTIFACT = "${artifactName}_${env.BRANCH_NAME}_${BUILD_NUMBER}.zip"
+      }
+      stages {
+          stage('Restore') {
+              steps {
+                  echo 'Restore Project'
+                  sh 'dotnet clean'
+                  sh 'dotnet restore --no-cache'
+              }
+          }
+          stage('Test'){
+              when{
+                  anyOf{
+                      changeset "${project}/**/*"
+                      changeset "${project}Cache/**/*"
+                      changeset "${project}Services/**/*"
+                      changeset "${project}Test/**/*"
+                      withTest
+                  }
+              }
+              steps {
+                  echo 'Testing..'
+                  sh "dotnet test ${PATH_TEST}"
+              }
+          }
+          stage('Build'){
+              when{
+                  anyOf{
+                      branch 'develop'
+                      branch 'stage'
+                      branch 'main'
+                  }
+                  anyOf{
+                      changeset "${project}/**/*"
+                      changeset "${project}Cache/**/*"
+                      changeset "${project}Services/**/*"
+                  }
+              }
+              steps {
+                  echo 'Build..'
+                  sh "dotnet publish -c Release ${PATH_PRJ}"
+                  zip zipFile: "${ARTIFACT}", overwrite: true, archive: false, dir: "${PATH_PUB}"
+                  archiveArtifacts artifacts: "${ARTIFACT}", fingerprint: true
+              }
+          }
+      }
+      post {
+        always {
+            sendTelegramNotification(TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL)
+        }
+    }
+  }
+}
