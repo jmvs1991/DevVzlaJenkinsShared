c9cd3448553bb94ee3e9baf70b53e335d133cc0b
José Vásquez <josevasquezc6@gmail.com>
2023-03-30 22:48:05 -0300

    Change forma JSOn

--- vars/sendTelegramNotification.groovy
+++ vars/sendTelegramNotification.groovy
@@ -22,11 +22,19 @@ def call() {
 
         def url = "https://api.telegram.org/bot${telegramBotToken}/sendMessage"
         
-        def body = {
-          chat_id: "${telegramChannel}",
-          text: "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
-          parse_mode: "Markdown"
-        }
+        // def body = {
+        //   chat_id: "${telegramChannel}",
+        //   text: "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
+        //   parse_mode: "Markdown"
+        // }
+
+        def body = """
+                        {
+                          "chat_id": "${telegramChannel}",
+                          "text": "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
+                          "parse_mode": "Markdown"
+                        }
+                    """
 
         def response = sh script: "curl -X POST -H 'Content-Type: application/json' -d '${body.encodeJson()}' ${url}", returnStdout: true
 
