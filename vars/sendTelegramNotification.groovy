312829131ef5b3bad7848099fb8916338dc086cc
José Vásquez <josevasquezc6@gmail.com>
2023-03-30 21:17:17 -0300

    Declaring a sendTelegramNotification

---
+++ vars/sendTelegramNotification.groovy
@@ -0,0 +1,18 @@
+def call() {
+    def telegramApi = credentials('telegram_api')
+    def telegramBotToken = credentials('telegram_bot_token')
+    def telegramChannel = credentials('telegram_channel_id')
+
+    def body = """
+                    {
+                      "chat_id": "${telegramChannel}",
+                      "text": "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
+                      "parse_mode": "Markdown"
+                    }
+                """
+    
+    httpRequest url: "${telegramApi}/${telegramBotToken}/sendMessage", 
+                        httpMode: 'POST',
+                        contentType: 'APPLICATION_JSON',
+                        requestBody: body
+}
