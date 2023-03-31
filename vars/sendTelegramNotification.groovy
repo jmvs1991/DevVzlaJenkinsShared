cfc09c60c0cd536585816f418bbf99be4e79a4b9
José Vásquez <josevasquezc6@gmail.com>
2023-03-30 22:45:19 -0300

    Parse Json

--- vars/sendTelegramNotification.groovy
+++ vars/sendTelegramNotification.groovy
@@ -23,9 +23,9 @@ def call() {
         def url = "https://api.telegram.org/bot${telegramBotToken}/sendMessage"
         
         def body = {
-          chat_id: telegramChannel,
-           text: "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
-          parse_mode: "Markdown"
+          "chat_id": "${telegramChannel}",
+          "text": "The job *${JOB_NAME}* Nr. *${BUILD_NUMBER}* is finished. \n\n *branch: ${env.BRANCH_NAME}* \n\n *result: ${currentBuild.result}* \n\n [Job Url](${BUILD_URL})",
+          "parse_mode": "Markdown"
         }
 
         def response = sh script: "curl -X POST -H 'Content-Type: application/json' -d '${body.encodeJson()}' ${url}", returnStdout: true
