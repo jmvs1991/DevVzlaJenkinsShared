a747877bea6c59e9064f5bed721a3e35ffa80393
José Vásquez <48494398+jmvs1991@users.noreply.github.com>
2023-03-31 08:55:40 -0300

    middlewarePipeline file

---
+++ vars/middlewarePipeline.groovy
@@ -0,0 +1,34 @@
+def call(String project, String artifact){
+  pipeline{
+      agent any
+      environment{
+          PATH_PRJ = "./${project}/${project}.csproj"
+          PATH_PUB = "./${project}/bin/Release/net6.0/publish"
+          ARTIFACT = "artifact_${env.BRANCH_NAME}_${BUILD_NUMBER}.zip"
+      }
+      stages{
+          stage('Restore') {
+              steps {
+                  echo 'Restore Project'
+                  sh 'dotnet clean'
+                  sh 'dotnet restore --no-cache'
+              }
+          }
+          stage('Build') {
+              when{
+                  anyOf{
+                      branch 'develop'
+                      branch 'stage'
+                      branch 'main'
+                  }
+              }
+              steps {
+                  echo 'Build..'
+                  sh "dotnet publish -c Release ${PATH_PRJ}"
+                  zip zipFile: "${ARTIFACT}", overwrite: true, archive: false, dir: "${PATH_PUB}"
+                  archiveArtifacts artifacts: "${ARTIFACT}", fingerprint: true
+              }
+          }
+      }
+  }
+}
