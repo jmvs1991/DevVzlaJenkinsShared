a308eecf4b117cfb58cf62a9f2a5e4b644bda5ee
José Vásquez <48494398+jmvs1991@users.noreply.github.com>
2023-04-21 22:59:19 -0300

    Add artifacts

--- vars/angularPipeline.groovy
+++ vars/angularPipeline.groovy
@@ -1,9 +1,11 @@
-def call(){
+def call(String folder, String artifactName){
   pipeline{
       agent any
       environment{
           TELEGRAM_BOT_TOKEN = credentials('telegram_bot_token')
           TELEGRAM_CHANNEL = credentials('telegram_channel_id')
+          PATH_PUB = "./dist/${project}"
+          ARTIFACT = "${artifactName}_${env.BRANCH_NAME}_${BUILD_NUMBER}.zip"
       }
       tools {
         nodejs "NodeJS16"
@@ -25,6 +27,8 @@ def call(){
               steps {
                   echo "Building"
                   sh 'npm run build:develop'
+                  zip zipFile: "${ARTIFACT}", overwrite: true, archive: false, dir: "${PATH_PUB}"
+                  archiveArtifacts artifacts: "${ARTIFACT}", fingerprint: true
               }
           }
       }
