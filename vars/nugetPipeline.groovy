5d13c8a56012e8d444852e06cb3002b79dad5719
José Vásquez <josevasquezc6@gmail.com>
2023-06-06 22:27:27 -0300

    TL: Addon nuget jenkins

---
+++ vars/nugetPipeline.groovy
@@ -0,0 +1,40 @@
+def call(String project){
+  pipeline {
+      agent any
+      environment{
+          TELEGRAM_BOT_TOKEN = credentials('telegram_bot_token')
+          TELEGRAM_CHANNEL = credentials('telegram_channel_id')
+          PATH_PRJ = "./${project}/${project}.csproj"
+      }
+      stages {
+          stage('Restore') {
+              steps {
+                  echo 'Restore Project'
+                  sh 'dotnet clean'
+                  sh 'dotnet restore --no-cache'
+              }
+          }
+          stage('Build'){
+              when{
+                  anyOf{
+                      branch 'develop'
+                      branch 'stage'
+                      branch 'main'
+                  }
+                  anyOf{
+                      changeset "${project}/**/*"
+                  }
+              }
+              steps {
+                  echo 'Build..'
+                  sh "dotnet build -c Release ${PATH_PRJ}"
+              }
+          }
+      }
+      post {
+        always {
+            sendTelegramNotification(TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL)
+        }
+    }
+  }
+}
