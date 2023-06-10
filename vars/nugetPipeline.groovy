d6868ddacd87f46df47466ca96f13b818accc3a1
José Vásquez <josevasquezc6@gmail.com>
2023-06-09 23:45:52 -0300

    TL: Remove comments

--- vars/nugetPipeline.groovy
+++ vars/nugetPipeline.groovy
@@ -29,11 +29,11 @@ def call(String project, String folder){
             }
         }    
         stage('Restore') {
-            // when{
-            //     anyOf{
-            //         changeset "${folder}/**/*"
-            //     }
-            // }
+            when{
+                anyOf{
+                    changeset "${folder}/**/*"
+                }
+            }
             steps {
                 echo 'Restore Project'
                 sh 'dotnet clean'
