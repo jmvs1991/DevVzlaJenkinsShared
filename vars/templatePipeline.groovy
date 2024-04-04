120408d9bed306bde758ff15d6beaba52f03bf98
José Vásquez <48494398+jmvs1991@users.noreply.github.com>
2024-04-04 00:06:21 -0300

    Update templatePipeline.groovy

--- vars/templatePipeline.groovy
+++ vars/templatePipeline.groovy
@@ -28,7 +28,7 @@ def call() {
                 }
                 steps {
                     echo 'Pack..'
-                    sh "dotnet pack -c Release ./ --output nupkgs"
+                    sh "dotnet pack -c Release . --output nupkgs"
                 }
             }
             stage('Publish') {
