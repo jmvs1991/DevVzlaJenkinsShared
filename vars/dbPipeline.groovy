54aa0ee254c10dd18e975c85f2e587fc22d9e15a
José Vásquez <48494398+jmvs1991@users.noreply.github.com>
2024-07-06 15:29:53 -0300

    Update dbPipeline.groovy

--- vars/dbPipeline.groovy
+++ vars/dbPipeline.groovy
@@ -1,6 +1,13 @@
 def call() {
     pipeline {
         agent any
+        environment {
+            TELEGRAM_BOT_TOKEN = credentials('telegram_bot_token')
+            TELEGRAM_CHANNEL = credentials('telegram_channel_id')
+            AWS_CODE_ARTIFACT_DOMAIN = credentials('aws-code-artifact-domain')
+            AWS_CODE_ARTIFACT_DOMAIN_OWNER = credentials('aws-code-artifact-domain-owner')
+            AWS_DEFAULT_REGION = credentials('aws-default-region')
+        }
         stages {
             stage('Input Data') {
                 steps {
