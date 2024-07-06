ac308b4fdc8ecf426f32682c1848d190bbab058d
José Vásquez <48494398+jmvs1991@users.noreply.github.com>
2024-07-06 16:34:08 -0300

    Update dbPipeline.groovy

--- vars/dbPipeline.groovy
+++ vars/dbPipeline.groovy
@@ -12,18 +12,24 @@ def call() {
             stage('Input Data') {
                 steps {
                     script {
-                        def userInput = input(
-                            id: 'userInput', message: 'Por favor, proporciona la siguiente información:', parameters: [
+                        try {
+                            timeout(time: 2, unit: 'MINUTES') {
+                                def userInput = input(id: 'userInput', message: 'Por favor, proporciona la siguiente información:', parameters: [
                                 string(name: 'DATA_SOURCE', description: 'Ip de la base de datos', defaultValue: ''),
                                 string(name: 'USER', description: 'Usuario', defaultValue: ''),
                                 string(name: 'PASSWORD', description: 'Password', defaultValue: ''),
                                 booleanParam(name: 'PROCEED', description: '¿Deseas continuar con el proceso?', defaultValue: true)
-                            ]
-                        )
-                        env.DATA_SOURCE = userInput.DATA_SOURCE
-                        env.USER = userInput.USER
-                        env.PASSWORD = userInput.PASSWORD
-                        env.PROCEED = userInput.PROCEED
+                            ])
+                                env.DATA_SOURCE = userInput.DATA_SOURCE
+                                env.USER = userInput.USER
+                                env.PASSWORD = userInput.PASSWORD
+                                env.PROCEED = userInput.PROCEED
+                            }   
+                        } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+                            echo "No se proporcionó la información dentro del tiempo límite. Abortando..."
+                            currentBuild.result = 'ABORTED'
+                            return
+                        }
                     }
                 }
             }
